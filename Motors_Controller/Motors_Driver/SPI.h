40e76fb69ded287d6c7c2dc7b8501f8399aee3c9
AhmedAshraf156 <122101238+AhmedAshraf156@users.noreply.github.com>
2024-07-31 00:01:35 +0300

    Add files via upload

---
+++ Motors_Controller/Motors_Driver/SPI.h
@@ -0,0 +1,19 @@
+#ifndef SPI_H_
+#define SPI_H_
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#define F_CPU 8000000UL
+#include "StandredMacros.h"
+void SPI_init_Master(void);
+void SPI_init_Slave(void);
+void SPI_sendByte(unsigned char data);
+unsigned char SPI_recieveByte(void);
+void SPI_sendString(const unsigned char *Str);
+void SPI_receiveString(char *Str);
+
+
+
+
+#endif /* SPI_H_ */
---
+++ Motors_Controller/Motors_Driver/timer.h
@@ -0,0 +1,20 @@
+#ifndef TIMER_H_
+#define TIMER_H_
+
+/************** Includes & Definitions *******************/
+
+#include <avr/io.h>
+#include "StandredMacros.h"
+
+/********************* ProtoTypes ************************/
+void timer0_init(void);
+void timer2_init(void);
+
+void timer0_low();
+void timer0_med();
+void timer0_high();
+void timer2_low();
+void timer2_med();
+void timer2_high();
+
+#endif 
